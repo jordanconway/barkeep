14c082f3114b838fbd12e1bf108db1e92cc2c795
jordanconway <jordan@jordanconway.com>
2014-05-05 10:05:25 -0400

    initial commit, basic sinatra webapp barkeep

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ barkeep
 =======
 
 Bar Inventory Webapp
+
+reqires the following gems:
+rubygems
+sinatra
+data_mapper
+do_sqlite3
+
+usage:
+ruby barkeep.rb
+
+TODO:
+add updates and deletes
+add booze type, dropdown selects, etc
+scrape name, size, etc from url (probably something better than saq)
---
+++ barkeep.rb
@@ -0,0 +1,73 @@
+require 'rubygems'
+require 'sinatra'
+require 'data_mapper'
+require 'do_sqlite3'
+
+enable :sessions
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/barkeep.db")
+
+class BoozeBottle
+    include DataMapper::Resource
+    property :id, Serial
+    property :name, Text, :required => true
+    property :size, Text, :required => true
+    property :ammount, Text, :required => true
+    property :saqurl, Text
+end
+
+DataMapper.finalize.auto_upgrade!
+
+# Do we need authentication?? Probably not yet, but I'll leave it in.
+helpers do
+
+    include Rack::Utils
+    alias_method :h, :escape_html
+
+    def protected!
+        unless authorized?
+            response['WWW-Authenticate'] = %(Basic realm="Restricted Area")
+            throw(:halt, [401, "Not authorized\n"])
+        end
+    end
+
+    def authorized?
+        @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+        @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == ['admin', 'admin']
+    end
+
+end
+
+get '/' do
+    @title = 'Barkeep! Simple booze inventory management'
+    erb :home
+end
+
+post '/' do
+    a = BoozeBottle.new
+    a.name = params[:name]
+    a.size= params[:size]
+    a.ammount = params[:ammount] 
+    a.saqurl= params[:saqurl] 
+    a.save
+    session[:number] = a.id
+    redirect to '/done'
+end
+
+get '/done' do
+    @boozebtl = BoozeBottle.get(session[:number])
+    @title = "done"
+    erb :done
+end
+
+get '/all' do
+    @title = 'all'
+    @boozebtl = BoozeBottle.all :order => :id.desc
+    erb :all
+end
+
+get '/:id' do
+    @boozebtl = BoozeBottle.get params[:id]
+    @title = "requested"
+    erb :show
+end
